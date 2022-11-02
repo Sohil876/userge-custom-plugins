526fb6e9ab7a0be4c2221096b2acdd6a52c5dce6
Sohil876 <sohil876@gmail.com>
2022-11-02 22:20:08 +0530

    l4wgen: Set referer to avoid 403 errors

--- plugins/custom/l4wgen/__main__.py
+++ plugins/custom/l4wgen/__main__.py
@@ -10,6 +10,8 @@ from userge import userge, Message
 
 user_agent = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36 OPR/72.0.3815.465 (Edition Yx GX)",
+    "Referer": "https://ww1.live4wap.xyz",
+    "Accept-encoding": "identity"
 }
 
 
