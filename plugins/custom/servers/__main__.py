049b322f28b41e5a8b8e359af3dd64fb2d772e8e
Sohil876 <sohil876@gmail.com>
2023-07-15 16:56:24 +0530

    servers: Update useragent, use referer header

--- plugins/custom/servers/__main__.py
+++ plugins/custom/servers/__main__.py
@@ -8,23 +8,23 @@ from bs4 import BeautifulSoup
 from userge import userge, Message #get_collection, config
 
 
-user_agent = {
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36 OPR/72.0.3815.465 (Edition Yx GX)",
-}
 workers_url="https://seep.eu.org"
+
 
 async def grab(host):
     async with ClientSession() as session:
-        async with session.get(f"{workers_url}/{host}/create-vps", headers=user_agent) as response:
-            if response.status != 200: return f"Something went wrong, aiohttp could not connect to server. Error Code: {response.status}"
+        async with session.get(f"{workers_url}/{host}/create-vps", headers={ "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36 OPR/72.0.3815.465 (Edition Yx GX)", "Referer": "{host}" }) as response:
+            if response.status != 200:
+                return f"Something went wrong, aiohttp could not connect to server. Error Code: {response.status}"
             soup = BeautifulSoup(await response.text(), "html.parser")
             found = soup.find(id="datacenter").find_all("option")[1:]
-            result = ""
             if len(found) == 0:
                 return "No free seats, check back later."
-            for i in found:
-                result += f"{i.text}\n"
-            return result
+            else:
+                result = ""
+                for i in found:
+                    result += f"{i.text}\n"
+                return result
 
 
 @userge.on_cmd(
